62ec7efeef61df986a285027d8ac60c3a32c7ab6
user1 <user1@user1-PC>
2018-10-15 13:42:55 +0900

    payment에 티켓모양 UI 수정

--- KHCinema/src/com/mini/khcinema/view/Payment.java
+++ KHCinema/src/com/mini/khcinema/view/Payment.java
@@ -14,6 +14,8 @@ import javax.swing.JPanel;
 
 import com.mini.khcinema.controller.PaymentController;
 import com.mini.khcinema.model.MoveReserve;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class Payment extends JPanel {
 
@@ -22,27 +24,30 @@ public class Payment extends JPanel {
 
 	public Payment(String movititle, String date, String movitime, String[] seat, /*int seatNum,*/ int personNum,
 			String myID, JPanel mf) {
+		setBackground(Color.LIGHT_GRAY);
+		setForeground(new Color(0, 0, 0));
 		setLayout(null);
 
 		this.pm = this;
-		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 10, 10);
-		add(panel);
-		panel.setLayout(null);
 		JLabel info = new JLabel("Reservation info");
-		info.setBounds(199, 415, 463, 43);
+		info.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		info.setBounds(233, 436, 268, 27);
 
 		ArrayList seatList = new ArrayList();
 		for (int i = 0; i < personNum; i++) {
 			seatList.add(seat[i]);
 		}
-		info.setText(movititle + " " + date + " 상영시간" + movitime + "분 " + "구매자:" + myID + " " + personNum + "명 좌석 "
-				+ seatList + "");
-
+		info.setText(movititle + " " + date + " 상영시간" + movitime + "분");
 		add(info);
+		
+		JLabel payinfo = new JLabel("payinfo");
+		payinfo.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+		payinfo.setText("구매자:" + myID + " " + personNum + "명 좌석 "+ seatList + "");
+		payinfo.setBounds(233, 465, 253, 15);
+		add(payinfo);
 
 		JLabel lblTitle = new JLabel("예매 정보 확인!");
-		lblTitle.setFont(new Font("굴림", Font.PLAIN, 40));
+		lblTitle.setFont(new Font("맑은 고딕", Font.PLAIN, 40));
 		lblTitle.setBounds(253, 29, 293, 86);
 		add(lblTitle);
 
@@ -59,9 +64,18 @@ public class Payment extends JPanel {
 		Image changedImg = originImg.getScaledInstance(270, 300, Image.SCALE_SMOOTH);
 		ic = new ImageIcon(changedImg);
 		JLabel lblNewLabel = new JLabel(ic);
+		lblNewLabel.setBackground(SystemColor.inactiveCaption);
 		lblNewLabel.setBounds(263, 102, 268, 306);
 		add(lblNewLabel);
-
+		
+		ImageIcon ic2 = new ImageIcon("Images/ticket.png");
+		Image originImg2 = ic2.getImage();
+		Image chagedImg2 = originImg2.getScaledInstance(400, 140, Image.SCALE_SMOOTH);
+		ic2 = new ImageIcon(chagedImg2);
+		JLabel model_ticket = new JLabel(ic2);
+		model_ticket.setBounds(211, 393, 408, 130);
+		add(model_ticket);
+		
 		paybtn.addActionListener(new ActionListener() {
 
 			@Override
