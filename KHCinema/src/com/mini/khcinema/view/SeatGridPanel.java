a4df5f132a52f53f453681bcca161289f7b4fa06
sinna94 <sinna94@naver.com>
2018-10-16 10:28:00 +0900

    클래스 이름 수정, 리뷰 평점 기능 추가

--- KHCinema/src/com/mini/khcinema/view/SeatGridPanel.java
+++ KHCinema/src/com/mini/khcinema/view/SeatGridPanel.java
@@ -16,7 +16,7 @@ import com.mini.khcinema.model.MovieReserve;
 public class SeatGridPanel extends JPanel {
 
 	boolean seatflag[][] = new boolean[7][8];
-	int select = 0;
+	int select = 0;	//좌석이 선택될 때 마다 증가시킴
 
 	public SeatGridPanel(String title, String date, String movitime, int count) {
 		setSize(20, 20);
@@ -40,16 +40,18 @@ public class SeatGridPanel extends JPanel {
 				seat[i][j].addMouseListener(new MouseAdapter() {
 					@Override
 					public void mouseClicked(MouseEvent e) {
-
-						if (e.getButton() == 1 && seatflag[r][c] == false /* && select>=count */) {
+						//마우스 클릭이벤트를 받으면서 flag 값이 좌석 비었음 일 때 
+						if (e.getButton() == 1 && seatflag[r][c] == false) {
+							//전달 받은 인원수가 선택된 좌석보다 많을 경우 이벤트 처리
 							if (select >= count) {
 								JOptionPane.showMessageDialog(null, "선택 가능한 좌석을 초과했습니다.");
 							} else {
+								//해당좌석의 배경을 검게 만들어 체크된 표시를 해준다.
 								seat[r][c].setBackground(Color.black);
 								seatflag[r][c] = true;
 								select++;
 							}
-						} else if (e.getButton() == 1 && seatflag[r][c] == true /* && select>0 */) {
+						} else if (e.getButton() == 1 && seatflag[r][c] == true) {
 							seat[r][c].setBackground(Color.white);
 							seatflag[r][c] = false;
 							select--;
@@ -65,22 +67,27 @@ public class SeatGridPanel extends JPanel {
 
 		// 예매정보를 불러와서 예약된 좌석은 비활 처리하는 로직
 		ArrayList check = new ArrayList();
+		//getMoveReserve()를 호출하여 reserve.txt 파일을 불러와 현재 예매상태를 불러오고 그걸 check에 넣는다.
 		check = new PaymentController().getMoveReserve();
 		try {
 			for (int i = 0; i < check.size(); i++) {
+				//for문을 통해 check에 있는 객체를 뽑아낸다.
 				MovieReserve m = (MovieReserve) check.get(i);
-
+				//reserve.txt에 있는 영화 제목과 날짜,시간이 같은 경우의 set데이터를 배열로 받는다.
 				if (m.getMoviTitle().equals(title) && m.getDate().equals(date) && m.getMoviTime().equals(movitime)) {
 					String[] checkseat = m.getSet();
 					for (int j = 0; j < checkseat.length; j++) {
+						//받은 배열 데이터는 A5,D3 이런식으로 저장되었기 때문에 substring으로 행과 열로 나눠준다.
 						String checkrow = checkseat[j].substring(0, 1);
 						String checkcol = checkseat[j].substring(1, 2);
+						
+						//받은 행과 열을 원래의 데이터로 복원하는 작업
 						char temp = checkrow.charAt(0);
 						int row = (int) temp;
 						int col = Integer.parseInt(checkcol);
 						row = row - 65;
 						--col;
-
+						//복원된 데이터를 넣어서 비활성화 해준다.
 						seat[row][col].setEnabled(false);
 					}
 				}
