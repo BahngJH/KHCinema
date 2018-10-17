e8dd6ed2b3ae7fe24f4a249623416a6276416b0b
BahngJH <wpxm2003@naver.com>
2018-10-17 10:08:50 +0900

    스타트패널에 현재 시각 구현

---
+++ KHCinema/src/com/mini/khcinema/controller/NowTimeController.java
@@ -0,0 +1,40 @@
+package com.mini.khcinema.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JLabel;
+
+import com.mini.khcinema.view.StartPanel;
+
+public class NowTimeController extends Thread {
+	
+	JLabel l;
+	//이 과정을 거쳐 줘야 주소값이 넘어가고 바로 하려고 하면 넘어가지 않는다.
+	public NowTimeController(JLabel l) {
+		this.l = l;
+	}
+	
+	@Override
+	public void run() {
+		while(true) 
+		{
+			SimpleDateFormat timeform = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			
+			Date time = new Date();
+			
+			String NowTime = timeform.format(time);
+			
+			try {
+				Thread.sleep(1000);
+				l.setText(NowTime);
+				
+				System.out.println("현재시각:"+NowTime);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
--- KHCinema/src/com/mini/khcinema/view/StartPanel.java
+++ KHCinema/src/com/mini/khcinema/view/StartPanel.java
@@ -2,9 +2,15 @@ package com.mini.khcinema.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import com.mini.khcinema.controller.NowTimeController;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class StartPanel extends JPanel {
 	public StartPanel() {
@@ -13,9 +19,22 @@ public class StartPanel extends JPanel {
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(199, 240, 253));
 		add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
 		
 		JLabel lblKh = new JLabel("\uC548\uB155\uD558\uC138\uC694 KH\uC2DC\uB124\uB9C8\uC785\uB2C8\uB2E4.");
+		lblKh.setBounds(273, 54, 156, 15);
 		panel.add(lblKh);
+		
+		JLabel TimeLabel = new JLabel("timelabel");
+		TimeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		TimeLabel.setFont(new Font("Arial Black", Font.PLAIN, 25));
+		TimeLabel.setBounds(257, 258, 313, 43);
+		panel.add(TimeLabel);
+		
+		//TimeLabel.setFont(new Font("맑은 고딕",Font.BOLD,30));
+		NowTimeController nowtime = new NowTimeController(TimeLabel);
+		nowtime.start();
+		
 	}
-
+	
 }
